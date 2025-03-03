7f117725822b041286872931de6c849fecd67513
giorgiameffe <giorgiameffe03@gmail.com>
2025-03-03 18:47:23 +0100

    feat: add saluti exercise

--- 01_saluti/script.js
+++ 01_saluti/script.js
@@ -5,9 +5,15 @@ const userName = 'Mario';
 
 // Dichiara la funzione qui.
 
+function string (word){
+
+    const helloPlusUser = word + ' ' + userName;
+    return helloPlusUser;
+
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(string('Ciao'));
 
 //Risultato atteso se si passa 'Mario': // ciao Mario

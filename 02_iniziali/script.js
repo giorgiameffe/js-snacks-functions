20fb666826de4f1aa885e89fe30942e11bed4f59
giorgiameffe <giorgiameffe03@gmail.com>
2025-03-04 00:44:26 +0100

    feat: add arrow function iniziali version exercise

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -7,6 +7,7 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 const newArray = [];
 
 function createNewArray (arrayOne,arrayTwo) {
+
     
     for (let i = 0; i < arrayOne.length; i++) { 
         
@@ -23,3 +24,30 @@ const result = createNewArray(names,newArray);
 console.log(result);
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
+
+
+
+// Versione con arrow fuctions
+
+const secondArray = [];
+
+// Dichiara la funzione qui.
+
+const singleLetterArray = (arrayOne,arrayTwo) => {
+
+    for (let i = 0; i < arrayOne.length; i++) { 
+        
+        const firstLetter = arrayOne[i].substring(0,1);
+        arrayTwo.push(firstLetter);
+    }
+    
+    return secondArray;
+}
+
+// Invoca la funzione qui e stampa il risultato in console
+
+const secondResult = createNewArray(names,secondArray);
+console.log(secondResult);
+
+//Risultato atteso: ["A", "L", "M", "A", "G", "A"]
+

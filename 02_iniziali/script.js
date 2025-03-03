8431c74c5be3a760902c54d744d84c935709d305
giorgiameffe <giorgiameffe03@gmail.com>
2025-03-03 22:59:28 +0100

    feat: add iniziali exercise

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -2,12 +2,24 @@
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
-
 // Dichiara la funzione qui.
 
+const newArray = [];
+
+function createNewArray (arrayOne,arrayTwo) {
+    
+    for (let i = 0; i < arrayOne.length; i++) { 
+        
+        const firstLetter = arrayOne[i].substring(0,1);
+        arrayTwo.push(firstLetter);
+    }
+    
+    return newArray;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+const result = createNewArray(names,newArray);
+console.log(result);
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]

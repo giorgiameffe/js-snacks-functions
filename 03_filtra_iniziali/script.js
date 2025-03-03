4e6d7e584a7db25cf87eb06da2166ca805316785
giorgiameffe <giorgiameffe03@gmail.com>
2025-03-04 00:18:55 +0100

    feat: add filtra iniziali exercise

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -5,9 +5,27 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 // Dichiara la funzione qui.
 
+const newArray = [];
+
+function firstLetterArrow (firstArray, letterString) {
+
+    for (let i = 0; i < firstArray.length; i++) {
+
+        if(firstArray[i].startsWith(letterString)) {
+
+            newArray.push(firstArray[i]);
+        }
+        
+    }
+
+    return newArray;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+const result = firstLetterArrow(names, 'A');
+console.log(result);
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
+
+

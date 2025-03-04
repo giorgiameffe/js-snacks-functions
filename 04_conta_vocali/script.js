0381126848ed70edf5364e8f35110019aede691c
giorgiameffe <giorgiameffe03@gmail.com>
2025-03-04 09:32:59 +0100

    feat: add conta-vocali exercise

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -2,12 +2,30 @@
 
 const word = 'javascript';
 
-
 // Dichiara la funzione qui.
 
+function howManyVowels(word) {
+
+    const volwes = ['a','e','i','o','u'];
+
+    let count = 0;
+
+    for (let i = 0; i < word.length; i++) {
+
+        for (let index = 0; index < volwes.length; index++) {
+
+            if (word[i]=== volwes[index]) 
+                
+                count++;
+        }
+    }
+
+    return count;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+const result = howManyVowels(word);
+console.log(result);
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
